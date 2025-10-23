5a9a6656cbae044bfc3f9e481f432c609b83197b
pammy <pbrebollido@up.edu.ph>
2025-10-23 18:29:44 +0800

    prepare deployment

--- sustainabytes_portfolio/next.config.ts
+++ sustainabytes_portfolio/next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  output: 'export',                // enables static export
-  images: { unoptimized: true },   // GitHub Pages can't optimize images
-  basePath: '/sustainabytes_portfolio',    // your repo name
-  assetPrefix: '/sustainabytes_portfolio/', // your repo name
+  output: "export",
+  images: { unoptimized: true },
+  basePath: isProd ? "/sustainabytes" : "",
+  assetPrefix: isProd ? "/sustainabytes/" : "",
 };
 
 export default nextConfig;
