ea939900c6457d57cafd0d8213d3e9b11a5b848e
wanderscrow <82goldencrown14@gmail.com>
2025-12-16 20:58:17 +0800

    text: add ML R&D drafted by Leo

--- sustainabytes_portfolio/app/discussion.tsx
+++ sustainabytes_portfolio/app/discussion.tsx
@@ -53,6 +53,249 @@ function Slide() {
   )
 }
 
+function Tables() {
+  return (
+    <div id="tableDisplay" className="flex flex-col md:flex-row">
+      <table className="items-center p-[10px] table-fixed w-[80vw]">
+        <thead>
+          <tr>
+            <td rowSpan={2}></td>
+            <th colSpan={3} className="bg-(--midground)">2025, Metro Manila</th>
+            <th colSpan={3} className="bg-(--midground)">2024, Rizal</th>
+            <th colSpan={3} className="bg-(--midground)">2023, Cebu</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>Actual</td>
+            <td>Predicted</td>
+            <td>%Error</td>
+
+            <td>Actual</td>
+            <td>Predicted</td>
+            <td>%Error</td>
+
+            <td>Actual</td>
+            <td>Predicted</td>
+            <td>%Error</td>
+          </tr>
+          <tr>
+            <td>Beans</td>
+            <td>108.896</td>
+            <td>102.654</td>
+            <td>5.732</td>
+            
+            <td>114.482</td>
+            <td>115.951</td>
+            <td>1.283</td>
+            
+            <td>108.896</td>
+            <td>97.803</td>
+            <td>10.187</td>
+          </tr>
+          <tr>
+            <td>Cabbages</td>
+            <td>73.783</td>
+            <td>77.719</td>
+            <td>5.334</td>
+            
+            <td>89.378</td>
+            <td>92.041</td>
+            <td>2.979</td>
+            
+            <td>73.783</td>
+            <td>66.927</td>
+            <td>9.292</td>
+          </tr>
+          <tr>
+            <td>Carrots</td>
+            <td>113.866</td>
+            <td>110.827</td>
+            <td>2.669</td>
+            
+            <td>110.892</td>
+            <td>114.181</td>
+            <td>2.966</td>
+            
+            <td>113.866</td>
+            <td>113.631</td>
+            <td>0.206</td>
+          </tr>
+          <tr>
+            <td>Eggs</td>
+            <td>8.603</td>
+            <td>8.326</td>
+            <td>3.220</td>
+            
+            <td>8.496</td>
+            <td>8.355</td>
+            <td>1.660</td>
+            
+            <td>8.603</td>
+            <td>8.807</td>
+            <td>2.371</td>
+          </tr>
+          <tr>
+            <td>Beef Chops</td>
+            <td>310.191</td>
+            <td>303.665</td>
+            <td>2.104</td>
+            
+            <td>390.879</td>
+            <td>397.482</td>
+            <td>1.689</td>
+            
+            <td>310.191</td>
+            <td>334.027</td>
+            <td>7.684</td>
+          </tr>
+          <tr>
+            <td>Chicken (Whole)</td>
+            <td>233.669</td>
+            <td>202.542</td>
+            <td>13.321</td>
+            
+            <td>186.921</td>
+            <td>189.79</td>
+            <td>1.535</td>
+            
+            <td>233.669</td>
+            <td>193.47</td>
+            <td>17.203</td>
+          </tr>
+          <tr>
+            <td>Pork</td>
+            <td>314.350</td>
+            <td>273.688</td>
+            <td>12.935</td>
+            
+            <td>326.508</td>
+            <td>332.821</td>
+            <td>1.933</td>
+            
+            <td>314.350</td>
+            <td>285.715</td>
+            <td>9.109</td>
+          </tr>
+          <tr>
+            <td>Onions</td>
+            <td>143.812</td>
+            <td>61.331</td>
+            <td>57.353</td>
+            
+            <td>111.074</td>
+            <td>118.043</td>
+            <td>6.274</td>
+            
+            <td>143.812</td>
+            <td>168.540</td>
+            <td>17.195</td>
+          </tr>
+          <tr>
+            <td>Potatoes</td>
+            <td>102.428</td>
+            <td>105.896</td>
+            <td>3.386</td>
+            
+            <td>124.010</td>
+            <td>126.747</td>
+            <td>2.207</td>
+            
+            <td>119.148</td>
+            <td>116.413</td>
+            <td>2.295</td>
+          </tr>
+          <tr>
+            <td>Rice</td>
+            <td>45.718</td>
+            <td>67.692</td>
+            <td>48.064</td>
+            
+            <td>47.571</td>
+            <td>44.802</td>
+            <td>5.821</td>
+            
+            <td>46.074</td>
+            <td>48.299</td>
+            <td>4.829</td>
+          </tr>
+          <tr>
+            <td>Tomatoes</td>
+            <td>79.293</td>
+            <td>103.767</td>
+            <td>30.865</td>
+            
+            <td>87.364</td>
+            <td>85.120</td>
+            <td>2.569</td>
+            
+            <td>83.964</td>
+            <td>86.211</td>
+            <td>2.676</td>
+          </tr>
+          <tr>
+            <td>Annual Per Capita Poverty Threshold</td>
+            <td>30759.000</td>
+            <td>30024.137</td>
+            <td>2.389</td>
+            
+            <td>39055.000</td>
+            <td>39462.190</td>
+            <td>1.043</td>
+            
+            <td>35605.000</td>
+            <td>35270.401</td>
+            <td>0.940</td>
+          </tr>
+          <tr>
+            <td>Poverty Incidence (%)</td>
+            <td>9.400</td>
+            <td>10.932</td>
+            <td>16.298</td>
+            
+            <td>5.600</td>
+            <td>6.180</td>
+            <td>10.357</td>
+            
+            <td>11.700</td>
+            <td>12.817</td>
+            <td>9.547</td>
+          </tr>
+          <tr>
+            <td>Magnitude of Poor Families (thousands)</td>
+            <td>5.600</td>
+            <td>12.687</td>
+            <td>126.554</td>
+            
+            <td>43.940</td>
+            <td>41.401</td>
+            <td>5.778</td>
+            
+            <td>157.460</td>
+            <td>171.391</td>
+            <td>8.847</td>
+          </tr>
+          <tr>
+            <td>Exchange Rate</td>
+            <td>57.228</td>
+            <td>56.974</td>
+            <td>0.444</td>
+            
+            <td>57.289</td>
+            <td>56.916</td>
+            <td>0.651</td>
+            
+            <td>55.631</td>
+            <td>55.935</td>
+            <td>0.546</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
 export default function Discussion() {
   return (
     <section id="discussion" className="min-h-screen snap-start flex flex-col justify-center items-center relative">
@@ -70,7 +313,24 @@ export default function Discussion() {
           </p>
           <br />
           <h2 className="h2">Machine Learning</h2>
-          <p className="p mt-6"> Machine Learning Discussion here! </p>
+          <p className="p mt-6">Detailed in the following tables are a given year and province, the actual values of a row in the dataset 
+            from that year and province (chosen at random if there are more than one), the values predicted by the model, and, underneath,
+             the average root mean squared error of all predictions across categories.</p>
+          <p className="p mt-6">Numbers are rounded to the nearest thousandths (i.e. to 3 decimal places). </p>
+          <Tables />
+          <p className="p mt-6">
+            In the case of “Metro Manila, 2025”, the predictions are not as accurate as hoped, with 7 out of 15 predictions having an 
+            error margin higher than 10%. However, for “Rizal, 2024” and “Cebu, 2023”, there are only 1 and 3 predictions, respectively 
+            with a similar error margin, and the others have less error. It is possible that the NCR’s data has a higher rate of 
+            variability or is affected more by other factors than data from other provinces of previous years, which could explain the 
+            higher error overall. Otherwise, an error margin of 10% or less represents in most cases a difference of up to 50 pesos, 
+            which does not seem unacceptable given how prices can vary throughout a year. 
+            </p>
+          <p className="p mt-6">More concerning is the error in predicting poverty incidence and number of poverty-stricken families, 
+            which is among the highest errors in each test comparison, and which represents hundreds or perhaps thousands of individuals. 
+            The weakness of the model’s prediction in this situation suggests, similarly to the hypothesis testing, that more factors 
+            than food pricing and exchange rates are at play when anticipating need, and a model intended to assist in doing so must be 
+            tuned to these other factors as well. </p>
         </div>
       </div>
     </section>
