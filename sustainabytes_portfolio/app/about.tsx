35ce63a0c5d39a73d74bd51f89c77f6769e617ee
wanderscrow <82goldencrown14@gmail.com>
2025-12-16 20:58:48 +0800

    fix: add sections for group members in the about section

--- sustainabytes_portfolio/app/about.tsx
+++ sustainabytes_portfolio/app/about.tsx
@@ -7,14 +7,64 @@ Idea for this: row members. for each member include
 
 "use client";
 
+import Image from "next/image";
+import { getImagePath } from "../utils/imagePath";
+
 export default function About() {
   return (
     <section id="team" className="h-screen snap-start flex flex-col justify-center items-center relative">
         <div className="z-10 text-center">
           <h1 className="h1">The Team</h1>
-          <p className="p px-[100px]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed metus urna, dignissim ac efficitur at, eleifend vel mi. Donec at elementum nisl. Cras in ullamcorper ex. Praesent porta eget justo pharetra tristique. Suspendisse dictum neque vel nisi tincidunt egestas. Sed fringilla nunc metus, quis vestibulum ligula pharetra vitae. Curabitur faucibus diam metus, sed dapibus diam tempus tempor. Nunc efficitur ex id sapien fringilla cursus. Nulla venenatis faucibus nulla. Sed dapibus a libero in congue. Nulla a diam eu enim convallis condimentum. Donec pellentesque maximus purus non sollicitudin. Phasellus vehicula ex tristique tristique placerat. Phasellus eu tempus diam.
-          </p>
+          <div className="flex flex-col md:flex-row items-center justify-center">
+            <div
+              id="about-pammy"
+              className="flex flex-col bg-(--background)/70 justify-center w-75 h-60 items-center p-5 rounded-3xl mx-3">
+              <Image
+                src={getImagePath("/1.svg")}
+                width={100}
+                height={100}
+                alt="pammy-pfp"
+                className="p-5"
+              />
+              <p className="p pb-5">Pammy Rebollido</p>
+            </div>
+            <div
+              id="about-soph"
+              className="flex flex-col bg-(--background)/70 justify-center w-75 h-60 items-center p-5 rounded-3xl mx-3">
+              <Image
+                src={getImagePath("/1.svg")}
+                width={100}
+                height={100}
+                alt="soph-pfp"
+                className="p-5"
+              />
+              <p className="p pb-5">Soph Santos</p>
+            </div>
+            <div
+              id="about-anton"
+              className="flex flex-col bg-(--background)/70 justify-center w-75 h-60 items-center p-5 rounded-3xl mx-3">
+              <Image
+                src={getImagePath("/1.svg")}
+                width={100}
+                height={100}
+                alt="anton-pfp"
+                className="p-5"
+              />
+              <p className="p pb-5">Anton Malagar</p>
+            </div>
+            <div
+              id="about-leo"
+              className="flex flex-col bg-(--background)/70 justify-center w-75 h-60 items-center p-5 rounded-3xl mx-3">
+              <Image
+                src={getImagePath("/1.svg")}
+                width={100}
+                height={100}
+                alt="leo-pfp"
+                className="p-5"
+              />
+              <p className="p pb-5">Leo Celestinos</p>
+            </div>
+          </div>
         </div>
     </section>
   );
